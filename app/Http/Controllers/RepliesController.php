2de7ca5d3aa7f1597d0664b5d1153053b6158fca
ArjanKamphuis <arjan_kamphuis@hotmail.com>
2019-08-27 01:49:05 +0200

    056 - Mentioned Users Notifications: Part 1

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use Exception;
 use App\Reply;
 use App\Thread;
+use App\User;
 use App\Http\Requests\CreatePostRequest;
+use App\Notifications\YouWereMentioned;
 use App\Rules\SpamFree;
 
 use Illuminate\Http\Request;
@@ -32,10 +34,20 @@ class RepliesController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store($channelId, Thread $thread, CreatePostRequest $form) {
-        return $thread->addReply([
+        $reply = $thread->addReply([
             'body' => request('body'),
             'user_id' => auth()->id()
         ])->load('owner');
+
+        preg_match_all('/\@([^\s\.]+)/', $reply->body, $matches);
+        
+        foreach ($matches[1] as $name) {
+            if ($user = User::whereName($name)->first()) {
+                $user->notify(new YouWereMentioned($reply));
+            }
+        }
+
+        return $reply;
     }
 
     /**
