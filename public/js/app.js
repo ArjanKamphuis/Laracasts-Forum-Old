f01242ae7820265a24daf173e1a78e2f21080abc
ArjanKamphuis <arjan_kamphuis@hotmail.com>
2019-09-02 14:29:10 +0200

    071 - Users Must Confirm Their Email Address: #1

--- public/js/app.js
+++ public/js/app.js
@@ -3199,7 +3199,7 @@ __webpack_require__.r(__webpack_exports__);
   props: ['message'],
   data: function data() {
     return {
-      body: '',
+      body: this.message,
       level: 'success',
       show: false
     };
@@ -3208,7 +3208,7 @@ __webpack_require__.r(__webpack_exports__);
     var _this = this;
 
     if (this.message) {
-      this.flash(this.message);
+      this.flash();
     }
 
     window.events.$on('flash', function (data) {
@@ -3217,8 +3217,11 @@ __webpack_require__.r(__webpack_exports__);
   },
   methods: {
     flash: function flash(data) {
-      this.body = data.message;
-      this.level = data.level;
+      if (data) {
+        this.body = data.message;
+        this.level = data.level;
+      }
+
       this.show = true;
       this.hide();
     },
