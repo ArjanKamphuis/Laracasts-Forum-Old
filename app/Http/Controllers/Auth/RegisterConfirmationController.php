fc2ce0023edbab643c4c104b7a0f674e71a9212e
ArjanKamphuis <arjan_kamphuis@hotmail.com>
2019-09-04 10:11:38 +0200

    074 - Email Confirmation Loose Ends

--- app/Http/Controllers/Auth/RegisterConfirmationController.php
+++ app/Http/Controllers/Auth/RegisterConfirmationController.php
@@ -1,8 +1,7 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\Auth;
 
-use Exception;
 use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -10,14 +9,13 @@ use App\Http\Controllers\Controller;
 class RegisterConfirmationController extends Controller
 {
     public function index() {
-        try {
-            User::where('confirmation_token', request('token'))
-                ->firstOrFail()
-                ->confirm();
-        } catch (Exception $e) {
-            return redirect(route('threads'))
-                ->with('flash', 'Unknown token.');
+        $user = User::where('confirmation_token', request('token'))->first();
+        
+        if (!$user) {
+            return redirect(route('threads'))->with('flash', 'Unknown token.');
         }
+
+        $user->confirm();
         return redirect(route('threads'))
             ->with('flash', 'Your account is now confirmed! You may post to the forum.');
     }
